3eb51cee80e4651cf4e1822d9df59f4cb8e96023
Krish Bhardwaj <180808748+krish031006-Me@users.noreply.github.com>
2026-02-05 22:33:50 +0530

    Created CreateRegisterView in views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializer import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.contrib.auth.models import User
 
-# Create your views here.
+# generics is kind of like an advance version of APIView we do not have to create 
+# methods for get, post, delete etc.
+# Instead we have two variables queryset and serializer_class 
+# queryset is used to pass the data and serializer_class is how shall we convert it to JSON
+class CreateRegisterView(generics.CreateAPIView): # This is the POST APIView
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny] # this specifies anyone can call this view
