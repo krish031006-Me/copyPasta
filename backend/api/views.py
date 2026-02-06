e6226a135c92cab9e5010a3cffac9277cdca703b
Krish Bhardwaj <180808748+krish031006-Me@users.noreply.github.com>
2026-02-06 22:53:42 +0530

    Created two new views for snippet creation and deletion.

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework import generics
-from .serializer import UserSerializer
+from .serializer import UserSerializer, SnippetModelSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.contrib.auth.models import User
+from models import Snippet
 
 # generics is kind of like an advance version of APIView we do not have to create 
 # methods for get, post, delete etc.
@@ -12,3 +14,33 @@ class CreateRegisterView(generics.CreateAPIView): # This is the POST APIView
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny] # this specifies anyone can call this view
+
+# This is the view for snippet management
+class CreateSnippetView(generics.ListCreateAPIView): # handles both GET and POST requests
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetModelSerializer
+    permission_classes = [IsAuthenticated]
+
+    # we created get_queryset rather than only using queryset cause we needed access to the user
+    def get_queryset(self):
+        user = self.request.user # This is how you fetch or get the user
+        return Snippet.objects.filter(author=user) # getting all the snippets by our user
+    
+    # also we can only view the notes written by us so we create a custom function like above to handle POST request too
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors) 
+            return Response(serializer.errors)
+
+# This is a view to delete a snippet
+class DeleteSnippetView(generics.DestroyAPIView):
+    serializer_class=SnippetModelSerializer
+    permission_classes=[IsAuthenticated]
+
+    # this is the custom method to handle the queryset
+    def get_queryset(self):
+        user = self.request.user
+        return Snippet.objects.filter(author=user)
+    # perform_destroy is the custom function in DestroyAPIView
