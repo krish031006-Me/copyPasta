5dbaf7ec1e185b7b4b2a05a4cec760378ccb7e8f
Krish Bhardwaj <180808748+krish031006-Me@users.noreply.github.com>
2026-02-07 23:51:35 +0530

    don't know what change i made!

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path("snippets/", CreateSnippetView.as_view(), name="snippet-list"),
     # <int:pk> is like a pathconverter tells to expect a number here
-    path("snippets/delete/<int:pk>/", DeleteSnippetView.as_view(), name="snippet-destroy") 
+    path("snippets/delete/<int:pk>/", DeleteSnippetView.as_view(), name="snippet-destroy")
 ]
