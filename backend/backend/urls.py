8ad4a015689e778c0202f4fdb5ed062cafd7c800
Krish Bhardwaj <180808748+krish031006-Me@users.noreply.github.com>
2026-02-07 23:52:02 +0530

    added the login route in the login file

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.views import CreateRegisterView
+from api.views import CreateRegisterView, CreateLoginView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/register/', CreateRegisterView.as_view(), name="register"),
+    path('api/login/', CreateLoginView.as_view(), name="login"),
     path('api/token/', TokenObtainPairView.as_view(), name="get_token"),
     path('api/token/refresh/', TokenRefreshView.as_view(), name="refresh"), 
     path('api-auth/', include("rest_framework.urls")), 
     path('api/', include("api.urls")) 
-]
+] 
