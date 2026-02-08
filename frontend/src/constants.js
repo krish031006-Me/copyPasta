df341b48998d56dcd1e8a006eba9fe7adba155dd
Krish Bhardwaj <180808748+krish031006-Me@users.noreply.github.com>
2026-02-08 23:12:26 +0530

    Made constants for ACCESS and REFRESH tokens.

---
+++ frontend/src/constants.js
@@ -0,0 +1,3 @@
+// This is the file created for storing the constants that would be used in other files. It's a standard procedure
+export const ACCESS_TOKEN = "access";
+export const REFRESH_TOKEN = "refresh";
