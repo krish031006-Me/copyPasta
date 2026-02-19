60de9354fde0cca97b6defbfaf7f15f503c39032
Krish Bhardwaj <180808748+krish031006-Me@users.noreply.github.com>
2026-02-19 21:00:51 +0530

    Build the homepage.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,89 @@
+import {useState, useEffect} from "react"
+import api from "../api"
+
 function Home(){
-    return <div>Home</div>
+    const [snippets, setSnippets] = useState([]);
+    const [notes, setNotes] = useState([]);
+    const [code, setCode] = useState("");
+    const [title, setTitle] = useState("");
+    const [language, setLanguage] = useState("");
+
+    
+    // This is the function to get the snippet
+    const getSnippet = async() => {
+        console.log("i'm trying to create one")
+        await api.get("api/snippets/")  
+        .then((res) => res = res.data)
+        .then((data) => {setSnippets(data); console.log(data)})
+        .catch((err) => alert(err))
+    }
+    
+    // THis is the function to delete node
+    const deleteSnippet = async(id) => {
+        await api.delete(`api/snippets/delete/${id}`)
+        .then((res) => {
+            if (res.status === 204){
+                alert("Item deleted.")
+            }else{
+                alert("Failed.")
+            }
+        }).catch((err) => alert(err))
+        // calling the getSnippet function to print the code after deletion
+        // we could simply delete stuff from state but we are not using it
+        getSnippet();
+    }
+
+    // This is the function to create a code snippet
+    const createSnippet = async(e) => {
+        console.log("i'm trying to create one")
+        e.preventDefault()
+        await api.post("api/snippets/", {
+            title:title,
+            code:code
+        }) 
+        .then((res) => {
+            if(res.status === 200){ // means the code snippet is created
+                alert("Snippet Created")
+            }
+        }).catch((err) => alert(err))
+        getSnippet();
+    }
+
+    // Using useEffect to call the functions
+    useEffect(() => {
+        getSnippet();
+    }, [])
+
+    return <div>
+        <div>
+            <h2>Your codes:</h2>
+        </div>
+        <h2>Create a snippet-</h2>
+        <form onSubmit={createSnippet}>
+            <label htmlFor="title">Title:</label>
+            <br />
+            <input 
+                required 
+                autoComplete="off"
+                type="text"
+                name="title"
+                id="title"
+                onChange={(e) => setTitle(e.target.value)} 
+                value={title}
+            />
+            <br />
+            <textarea 
+                required
+                autoComplete="off"
+                type="text"
+                name="code"
+                value={code}
+                id="code"
+                onChange={(e) => setCode(e.target.value)} 
+            />
+            <input type="submit" value="Submit" />
+        </form>
+    </div>
 }
 
 export default Home
