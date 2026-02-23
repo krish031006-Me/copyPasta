2e1bd0a7514e12825d41e4e46f60acca493f064b
Krish Bhardwaj <180808748+krish031006-Me@users.noreply.github.com>
2026-02-23 19:43:51 +0530

    Boilerplate code for the sidebar.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.views import CreateRegisterView, CreateLoginView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
