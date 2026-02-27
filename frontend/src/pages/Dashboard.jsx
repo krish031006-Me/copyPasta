b2b64c28630606825e972b11660ddeedff46099d
Krish Bhardwaj <180808748+krish031006-Me@users.noreply.github.com>
2026-02-27 18:21:37 +0530

    Added a function to get counts and pass them down as props.

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,34 @@
-import SideBar from "../components/SideBar"
+import {SideBar} from "../components/SideBar"
+import {DashboardHeader} from "../components/DashboardHeader"
+import { StatsBar } from "../components/StatsBar"
 
 function Dashboard(){
+    // Using state to store the count
+    const [count, setCount] = useState({})
+        
+    // This is the function to get the count
+    async function LoadCounts() {
+        const types = ["all", "code", "links", "text", "favorites", "recent", "trash"]
+        results = {}
+        await Promise.all(
+            types.map(async (type) => {
+            const res = await api.get(`api/snippets/?type=${type}`)
+            results[type] = res.data.count
+            })
+        )
+        setCount(results)
+    }
+
+    // This is the react function to store all the counts of values in state 
+    useEffect(() => {
+        LoadCounts()
+    }, [])
+    
     return(
-        <SideBar></SideBar>
+        <div>
+            <SideBar count={count} refreshCount={LoadCounts}/>
+            <DashboardHeader count={count} /> 
+            <StatsBar count={count} /> 
+        </div>
     )
 }
